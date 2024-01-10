a68ea96ec958b7245c4bd32b97840685d1eece93
mith <itzmrmith@gmail.com>
2024-01-10 13:55:20 -0600

    comments

--- Python Warmup/Warmup 1.py
+++ Python Warmup/Warmup 1.py
@@ -1,13 +1,19 @@
+# import datetime library for getting month
 from datetime import datetime
 
+# ask for name and birthday month
 name = input("What is your name? ")
 month = input("What month were you born in? ")
 
 print("Hello, " + name + "!")
 
+# grab current date
 currentDate = datetime.now()
 
+# if the user inputs the month as a number that is the first condition
+# 2nd condition checks if they input the month name equals the current month name when program is ran
 if currentDate.month == month or currentDate.strftime("%B").lower() == month.strip().lower():
     print("Happy Birthday month!")
-    
+
+# Get length of user's name and output length.
 print(f"There are {len(name)} letters in your name")
--- Python Warmup/Warmup 2.py
+++ Python Warmup/Warmup 2.py
@@ -1,11 +1,16 @@
+# initialize an empty array
 classesTaken = []
 
+# Grab class count from user input, string -> int -> range so we can iterate over that.
 for classTaken in range(int(input("How many classes are you taking this semester? "))):
+    # grab class name
     localClass = input("Enter the name of your class: ")
+    # add class name to array
     classesTaken.append(localClass)
 
 print("The classes you are taking are:")
 
+# print out the class name by iterating over the array
 for className in classesTaken:
     print(className)
     
